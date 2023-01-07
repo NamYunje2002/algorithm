b653c011bf994f66a09b48357ed97e64333123dc
NamYunje2002 <119591226+NamYunje2002@users.noreply.github.com>
2023-01-07 10:23:04 +0900

    Update 14002.cpp

--- boj/14002.cpp
+++ boj/14002.cpp
@@ -1,4 +1,4 @@
-/* https://www.acmicpc.net/problem/14002 */
+// https://www.acmicpc.net/problem/14002
 
 #include <iostream>
 
@@ -13,11 +13,15 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 		dp[i] = 1;
-		for (int j = 0; j < i; j++) if (arr[i] > arr[j]) dp[i] = max(dp[i], dp[j] + 1);
+		for (int j = 0; j < i; j++) 
+			if (arr[i] > arr[j]) 
+				dp[i] = max(dp[i], dp[j] + 1);
 		length = max(dp[i], length);
 	}
 	cout << length << "\n";
 	l = length;
-	for (int i = n - 1; i > -1; i--) if (dp[i] == length) ans[--length] = arr[i];
+	for (int i = n - 1; i > -1; i--) 
+		if (dp[i] == length) 
+			ans[--length] = arr[i];
 	for (int i = 0; i < l; i++) cout << ans[i] << " ";
 }
