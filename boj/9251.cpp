579b9b418c13397fc945ba1a65e79d26a3fb20a6
NamYunje2002 <119591226+NamYunje2002@users.noreply.github.com>
2023-01-07 18:08:35 +0900

    Update 9251.cpp

--- boj/9251.cpp
+++ boj/9251.cpp
@@ -1,5 +1,3 @@
-// https://www.acmicpc.net/problem/9251
-
 #include <iostream>
 
 using namespace std;
@@ -12,13 +10,13 @@ int main() {
 	cin.tie(0);
 	cin >> s1 >> s2;
 
-	for (int i = 0; i < s1.size(); i++) {
-		for (int j = 0; j < s2.size(); j++) {
-			if (s1[i] == s2[j]) 
+	for (int i = 0; i < s2.size(); i++) {
+		for (int j = 0; j < s1.size(); j++) {
+			if (s1[j] == s2[i]) 
 				dp[i + 1][j + 1] = dp[i][j] + 1;
 			else 
 				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
 		}
 	}
-	cout << dp[s1.size()][s2.size()];
+	cout << dp[s2.size()][s1.size()];
 }
