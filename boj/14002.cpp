731369e3b0c7bfcafb61a6f4a4b669a4706cf85f
NamYunje2002 <119591226+NamYunje2002@users.noreply.github.com>
2023-01-10 21:13:57 +0900

    Update 14002.cpp

--- boj/14002.cpp
+++ boj/14002.cpp
@@ -10,20 +10,24 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n;
+
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 		dp[i] = 1;
+
 		for (int j = 0; j < i; j++) 
 			if (arr[i] > arr[j]) 
 				dp[i] = max(dp[i], dp[j] + 1);
-		
+
 		length = max(dp[i], length);
 	}
 	cout << length << "\n";
 	l = length;
+
+	// 수열 탐색
 	for (int i = n - 1; i > -1; i--) 
-		if (dp[i] == length) 
+		if (dp[i] == length)
 			ans[--length] = arr[i];
-	
+
 	for (int i = 0; i < l; i++) cout << ans[i] << " ";
 }
