5eed20669bf2ffeb1262ee53fb3e4b739185f51a
namyunje <119591226+NamYunje2002@users.noreply.github.com>
2023-01-28 14:04:21 +0900

    N과 M (3)

--- boj/15651.cpp
+++ boj/15651.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int n, m, arr[9];
 
-void solve(int c) {
-    if (c == m) {
+void solve(int k) {
+    if (k == m) {
         for(int i = 0; i < m; i++)
-            cout << arr[i] << ' ';
+            cout << arr[i] << " ";
         cout << "\n";
         return;
     }
     for (int i = 1; i <= n; i++) {
-        arr[c] = i;
-        solve(c + 1);
+        arr[k] = i;
+        solve(k + 1);
     }
 }
 
