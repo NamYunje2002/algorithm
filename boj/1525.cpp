a8e34b17ae3c08cdf188c5d47d62f483df2a952a
namyunje <119591226+NamYunje2002@users.noreply.github.com>
2023-02-14 09:50:58 +0900

    퍼즐

--- boj/1525.cpp
+++ boj/1525.cpp
@@ -1,7 +1,4 @@
-// https://www.acmicpc.net/problem/1525
-
 #include <iostream>
-#include <cmath>
 #include <queue>
 #include <set>
 using namespace std;
@@ -10,14 +7,15 @@ using namespace std;
 #define Y second
 
 int n, st, ans = -1;
-int dx[4] = { 1, -1, 0, 0 }, dy[4] = { 0, 0, 1, -1 };
+int p10[10], dx[4] = { 1, -1, 0, 0 }, dy[4] = { 0, 0, 1, -1 };
 queue<pair<int, int>> Q;
 set<int> vis;
 
 // 다음 숫자(바꾸려는 숫자) 찾기
 int getNxtNum(int num, int nxtIdx) {
-	for (int i = 8; i > nxtIdx; i--) 
+	for (int i = 8; i > nxtIdx; i--) {
 		num /= 10;
+	}
 	return num % 10;
 }
 
@@ -34,20 +32,24 @@ int getZeroIdx(int num) {
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
+	// 거듭제곱 계산
+	p10[0] = 1;
+	for (int i = 1; i < 9; i++) p10[i] = p10[i - 1] * 10;
+
 	for (int i = 8; i >= 0; i--) {
 		cin >> n;
-		st += n * pow(10, i);
+		st += n * p10[i];
 	}
 	Q.push({ st,0 });
 	vis.insert(st);
-
 	while (!Q.empty()) {
 		int cur = Q.front().X;
 		int cnt = Q.front().Y;
 		Q.pop();
-		
+
 		// 정리되었을 경우
-		if (cur == 123456780 && (ans == -1 || cnt < ans)) 
+		if (cur == 123456780 && (ans == -1 || cnt < ans))
 			ans = cnt;
 
 		int zeroIdx = getZeroIdx(cur);
@@ -69,11 +71,11 @@ int main() {
 			for (int j = 0; j < 9; j++) {
 				int lastNum = tmp % 10;
 				if (lastNum == 0) 
-					nxt += lastNum * pow(10, 8 - nxtIdx);
-				else if (lastNum == nxtNum)
-					nxt += lastNum * pow(10, 8 - zeroIdx);
+					nxt += lastNum * p10[8 - nxtIdx];
+				else if (lastNum == nxtNum) 
+					nxt += lastNum * p10[8 - zeroIdx];
 				else 
-					nxt += lastNum * pow(10, j);
+					nxt += lastNum * p10[j];
 				tmp /= 10;
 			}
 			if (vis.find(nxt) != vis.end()) continue;
