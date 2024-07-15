e81c6ce7a69cc91c4e37a5cd6813e45f378f2a05
namyunje <namyunje2002@gmail.com>
2024-07-15 17:06:08 +0900

    카드 구매하기

---
+++ BOJ/11052.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[1005], dp[1005];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+    for(int i = 1; i <= n; i++) {
+        dp[i] = arr[i];
+        for(int j = 1; j < i; j++) {
+            dp[i] = max(dp[i], dp[j]+dp[i-j]);
+        }
+    }
+    cout << dp[n];
+}
