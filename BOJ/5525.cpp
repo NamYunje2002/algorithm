f26a2fa896c968468ef02492f1c68e5dc937ea35
namyunje <namyunje2002@gmail.com>
2024-07-23 20:35:11 +0900

    IOIOI

--- BOJ/5525.cpp
+++ BOJ/5525.cpp
@@ -5,14 +5,21 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int ans = 0;
     int n, m; cin >> n >> m;
-    n = n*2+1;
+    n = n*2+1; 
     string s; cin >> s;
-    for(int i = 0; i <= m-n; i++) {
-        int chk = 0;
-        for(int j = 0; j < n; j++) if(j%2==0&&s[i+j]=='O'||j%2==1&&s[i+j]=='I') chk=1;
-        if(!chk) {
-            ans++;
+    for(int i = 0; i < m; i++) {
+        if(s[i] == 'O') continue;
+        int j = i;
+        for(; j+1 < m; j++) if(s[j] == s[j+1]) break;
+        if(i < j) {
+            int len = j-i+1;
+            if(len >= n) {
+                ans++;
+                len -= n;
+                ans += len/2;
+            }
         }
+        i = j;
     }
     cout << ans;
 }
