82f069e7e8b32c4b4027c062dcfda1267832caab
namyunje <namyunje2002@gmail.com>
2024-07-29 15:52:25 +0900

    이중 우선순위 큐

--- BOJ/7662.cpp
+++ BOJ/7662.cpp
@@ -1,155 +1,155 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
 int heapSize;
-ll mmHeap[1000005];
-int s1[15], s2[15];
+int mmHeap[1000005];
 
-int isMinLevel(int level) {
-    return (level % 2 == 1);
+int isMinLevel(int idx);
+void swap(int *a, int *b);
+void insertHeap(int num);
+void upHeap(int idx);
+void upHeapMax(int idx);
+void upHeapMin(int idx);
+void downHeap(int idx);
+int getMaxOrMinFromChild(int idx, bool isMax);
+int getMax();
+int getMin();
+void removeMax();
+void removeMin();
+
+int isMinLevel(int idx) {
+    return ((int)(log2(idx)) % 2 == 0);
 }
 
-void swap(ll *a, ll *b) {
-    ll tmp = *a;
+void swap(int *a, int *b) {
+    int tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-void upHeap(int idx, int level) {
-    if(idx/2 < 1) return;
-    if(isMinLevel(level)) {
+void insertHeap(int num) {
+    mmHeap[++heapSize] = num;
+    upHeap(heapSize);
+}
+
+void upHeap(int idx) {
+    if(idx < 2) return;
+    if(isMinLevel(idx)) {
         if(mmHeap[idx] > mmHeap[idx/2]) {
             swap(&mmHeap[idx], &mmHeap[idx/2]);
-            level--;
-            idx /= 2;
+            upHeapMax(idx/2);
+        } else {
+            upHeapMin(idx);
         }
     } else {
         if(mmHeap[idx] < mmHeap[idx/2]) {
             swap(&mmHeap[idx], &mmHeap[idx/2]);
-            level--;
-            idx /= 2;
-        }
-    }
-    while(idx/4 >= 1) {
-        int chk = 0;
-        if(isMinLevel(level)) {
-            if(mmHeap[idx] < mmHeap[idx/4]) {
-                swap(&mmHeap[idx], &mmHeap[idx/4]);
-                idx /= 4;
-            } else chk = 1;
+            upHeapMin(idx/2);
         } else {
-            if(mmHeap[idx] > mmHeap[idx/4]) {
-                swap(&mmHeap[idx], &mmHeap[idx/4]);
-                idx /= 4;
-            } else chk = 1;
+            upHeapMax(idx);
         }
-        if(chk) break;
     }
 }
 
-void downHeap(int idx, int level) {
-    while(idx*2 <= heapSize) {
-        int chk = 0;
-        if(isMinLevel(level)) {
-            int mnIdx = idx;
-            for(int i = idx; i <= idx*4+3 && heapSize; i++) {
-                if(mmHeap[i] < mmHeap[mnIdx]) mnIdx = i;
-            }
-            if(2*idx <= mnIdx <= 2*idx+1) {
-                swap(mmHeap[idx], mmHeap[mnIdx]);
-                idx = mnIdx;
-                level += 1;
-            }
-            else if(idx*4 <= mnIdx <= idx*4+3) {
-                swap(mmHeap[idx], mmHeap[mnIdx]);
-                idx = mnIdx;
-                if(mmHeap[idx] > mmHeap[idx/2]) swap(mmHeap[idx], mmHeap[idx/2]);
-                level += 2;
-            } else chk = 1;
+void upHeapMax(int idx) {
+    if(idx < 4) return;
+    if(mmHeap[idx] > mmHeap[idx/4]) {
+        swap(&mmHeap[idx], &mmHeap[idx/4]);
+        upHeapMax(idx/4);
+    }
+}
+
+void upHeapMin(int idx) {
+    if(idx < 4) return;
+    if(mmHeap[idx] < mmHeap[idx/4]) {
+        swap(&mmHeap[idx], &mmHeap[idx/4]);
+        upHeapMin(idx/4);
+    }
+}
+
+void downHeap(int idx) {
+    if(idx * 2 > heapSize) return;
+    
+    if(isMinLevel(idx)) {
+        int mIdx = getMaxOrMinFromChild(idx, false);
+        if(mIdx == idx) return;
+        if(mIdx >= idx*4) {
+            swap(&mmHeap[mIdx], &mmHeap[idx]);
+            if(mmHeap[mIdx] > mmHeap[mIdx/2]) swap(&mmHeap[mIdx], &mmHeap[mIdx/2]);
+            downHeap(mIdx);
         } else {
-            int mxIdx = idx;
-            for(int i = idx; i <= (idx*2+1)*2+1 && heapSize; i++) {
-                if(mmHeap[i] > mmHeap[mxIdx]) mxIdx = i;
-            }
-            if(2*idx <= mxIdx <= 2*idx+1) {
-                swap(mmHeap[idx], mmHeap[mxIdx]);
-                idx = mxIdx;
-                level += 1;
-            }
-            else if(idx*4 <= mxIdx <= (idx*2+1)*2+1) {
-                swap(mmHeap[idx], mmHeap[mxIdx]);
-                idx = mxIdx;
-                if(mmHeap[idx] < mmHeap[idx/2]) swap(mmHeap[idx], mmHeap[idx/2]);
-                level += 2;
-            } else chk = 1;
-        }
-        if(chk) break;
+            swap(&mmHeap[mIdx], &mmHeap[idx]);
+        }   
+    } else {
+        int mIdx = getMaxOrMinFromChild(idx, true);
+        if(mIdx == idx) return;
+        if(mIdx >= idx*4) {
+            swap(&mmHeap[mIdx], &mmHeap[idx]);
+            if(mmHeap[mIdx] < mmHeap[mIdx/2]) swap(&mmHeap[mIdx], &mmHeap[mIdx/2]);
+            downHeap(mIdx);
+        } else {
+            swap(&mmHeap[mIdx], &mmHeap[idx]);
+        }  
     }
 }
 
-void insertHeap(ll num) {
-    mmHeap[++heapSize] = num;
-    int level = 1;
-    for(int i = 14; i >= 0; i--) {
-        if(heapSize > s2[i]) {
-            level = i+2;
-            break;
-        }
+int getMaxOrMinFromChild(int idx, bool isMax) {
+    int rsl = idx;
+    int childIdxs[6] = { idx*2, idx*2+1, idx*4, idx*4+1, idx*4+2, idx*4+3 };
+    for(auto childIdx : childIdxs) {
+        if(childIdx > heapSize) continue;
+        if(isMax && mmHeap[childIdx] > mmHeap[rsl]) rsl = childIdx;
+        if(!isMax && mmHeap[childIdx] < mmHeap[rsl]) rsl = childIdx;
     }
-    upHeap(heapSize, level);
+    return rsl;
 }
 
-void removeMn() {
-    if(heapSize == 0) return;
-    mmHeap[1] = mmHeap[heapSize];
-    mmHeap[heapSize--] = 0;
-    downHeap(1, 1);
+int getMin() {
+    return mmHeap[1];
 }
 
-void removeMx() {
-    if(heapSize == 1) {
-        removeMn();
-    } else if(heapSize >= 2) {
-        int mxIdx = 2;
-        if(heapSize > 2) if(mmHeap[3] > mmHeap[2]) mxIdx = 3;
-        mmHeap[mxIdx] = mmHeap[heapSize];
-        mmHeap[heapSize--] = 0;
-        downHeap(mxIdx, 1); 
-    }
+int getMax() {
+    if(heapSize == 1) return mmHeap[1];
+    else if(heapSize == 2) return mmHeap[2];
+    else return max(mmHeap[2], mmHeap[3]);
+}
+
+void removeMin() {
+    if(heapSize < 1) return;
+    mmHeap[1] = mmHeap[heapSize--];
+    downHeap(1);
+}
+
+void removeMax() {
+    if(heapSize < 1) return;
+    int idx = 1;
+    if(heapSize == 2) idx = 2;
+    else idx = (mmHeap[2] > mmHeap[3] ? 2 : 3);
+    mmHeap[idx] = mmHeap[heapSize--];
+    downHeap(idx);
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    s1[0] = s2[0] = 1;
-    for(int i = 1; i < 15; i++) {
-        s1[i] = s1[i-1]*2;
-        s2[i] = s1[i-1]*2+s2[i-1];
-    }
     int t; cin >> t;
+    
     while(t--) {
         int k; cin >> k;
+        
         while(k--) {
-            string s; ll n;
+            string s; int n;
             cin >> s >> n;
+            
             if(s == "I") {
                 insertHeap(n);
-            } else {
-                if(n == 1) removeMx();
-                else removeMn();
+            }
+            else {
+                if(n == 1) removeMax();
+                else removeMin(); 
             }
         }  
-        if(heapSize == 0) cout << "EMPTY\n";
-        else {
-            int mxIdx = 1;
-            if(heapSize >= 2)  {
-                mxIdx = 2;
-                if(heapSize > 2) if(mmHeap[3] > mmHeap[2]) mxIdx = 3;
-            }
-            cout << mmHeap[mxIdx] << ' ' << mmHeap[1] << '\n';
-        }
-        fill(mmHeap, mmHeap+1000005, 0);
+        if(heapSize) cout << getMax() << ' ' << getMin() << '\n';
+        else cout << "EMPTY\n";
         heapSize = 0;
     }
 }
