a334316afb7f4c3df8252ebf8ca81c855e398b7e
namyunje <namyunje2002@gmail.com>
2024-09-05 19:30:57 +0900

    이진 검색 트리

--- BOJ/5639.cpp
+++ BOJ/5639.cpp
@@ -1,46 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int root, nodeCnt;
-int pArr[1000005];
-pair<int, int> cArr[1000005];
+int nodeCnt = -1;
+int arr[10005];
 
-int findParent(int n, int c) {
-    if (pArr[c] > n || pArr[c] == -1) return c;
-    return findParent(n, pArr[c]);
-}
-
-void dfs(int cur) {
-    if (cArr[cur].first) dfs(cArr[cur].first);
-    if (cArr[cur].second) dfs(cArr[cur].second);
-    cout << cur << "\n";
-}
-
-void build(int p) {
-    int cur; cin >> cur;
-    if (cin.eof()) return;
-    nodeCnt++;
-    if (p > cur) {
-        cArr[p].first = cur;
-        pArr[cur] = p;
-    } else {
-        int parent = findParent(cur, p);
-        while (cArr[parent].second) {
-            parent = cArr[parent].second;
-        }     
-        cArr[parent].second = cur;
-        pArr[cur] = parent;
-    }
-    build(cur);
+void solve(int st, int en) {
+    if (st >= en) return;
+    int idx = st;
+    while (idx < en) {
+        if (arr[++idx] > arr[st]) break;
+    } 
+    solve(st+1, idx);
+    solve(idx, en);
+    cout << arr[st] << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    cin >> root;
-    pArr[root] = -1;
-    if (!cin.eof()) {
-        nodeCnt++;
-        build(root);
-    }
-    dfs(root);
+    int n;
+    while(cin >> arr[++nodeCnt]);
+    solve(0, nodeCnt);
 }
