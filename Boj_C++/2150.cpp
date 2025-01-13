288fa3d349bc9cb798532e477315fe924b54346e
namyunje <namyunje2002@gmail.com>
2025-01-13 10:10:50 +0900

    Strongly Connected Component

--- Boj_C++/2150.cpp
+++ Boj_C++/2150.cpp
@@ -1,36 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int v, e, ans;
-int vis[10005], p[10005];
-vector<int> adj[10005];
-stack<int> st;
+int v, e, numCnt;
+int vis[10005];
+vector<pair<int, int> > vertexV;
+vector<int> adj[10005], rAdj[10005];
+vector<vector<int> > sccV;
 
-int solve(int cur) {
-    st.push(cur);
-    p[cur] = cur;
-    int parent = cur;
-    for (auto nxt : adj[cur]) {
-        if (!p[nxt]) {
-            parent = nxt;
-            break;
-        } else {
-            return solve(cur);
-        }
-    }
-    return parent;
+bool cmp(pair<int, int>& p1, pair<int, int>& p2) {
+	return p1.first > p2.first;
+}
+
+void dfs1(int cur) {
+	for (const auto& nxt : adj[cur]) {
+		if (vis[nxt]) continue;
+		vis[nxt] = 1;
+		dfs1(nxt);
+	}
+	vertexV[numCnt] = { numCnt + 1, cur };
+	numCnt++;
+}
+
+void dfs2(int cur, vector<int>& scc) {
+	for (const auto& nxt : rAdj[cur]) {
+		if (vis[nxt]) continue;
+		vis[nxt] = 1;
+		dfs2(nxt, scc);
+	}
+	scc.push_back(cur);
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    cin >> v >> e;
-    for (int i = 0; i < e; i++) {
-        int a, b; cin >> a >> b;
-        adj[a].push_back(b);
-    }
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> v >> e;
+	vertexV.resize(v);
+	while (e--) {
+		int a, b; cin >> a >> b;
+		adj[a].push_back(b);
+		rAdj[b].push_back(a);
+	}
 
-    for (int i = 1; i <= v; i++) {
-        if (vis[i]) continue;
-        int parent = solve(i);
-    }
+	for (int i = 1; i <= v; i++) {
+		if (vis[i]) continue;
+		vis[i] = 1;
+		dfs1(i);
+	}
+	
+	for (int i = 1; i <= v; i++) vis[i] = 0;
+	sort(vertexV.begin(), vertexV.end(), cmp);
+	for (int i = 0; i < v; i++) {
+		int cur = vertexV[i].second;
+		if (vis[cur]) continue;
+		vis[cur] = 1;
+		vector<int> subScc;
+		dfs2(cur, subScc);
+		sort(subScc.begin(), subScc.end());
+		sccV.push_back(subScc);
+	}
+	sort(sccV.begin(), sccV.end(), [](const vector<int>& a, const vector<int>& b) {
+		return a[0] < b[0];
+	});
+
+	cout << sccV.size() << "\n";
+	for (const auto& curScc : sccV) {
+		for (int num : curScc) {
+			cout << num << " ";
+		}
+		cout << "-1\n";
+	}
 }
