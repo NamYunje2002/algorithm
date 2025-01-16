415e4d1b229241545b911dc8c9521cb3cb53befb
namyunje <namyunje2002@gmail.com>
2025-01-16 19:31:56 +0900

    모두의 마블

---
+++ Boj_C++/12845.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    int ans = 0;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n - 1; i++) {
+        ans += v[i] + v[n - 1];
+    }
+    cout << ans;
+}
